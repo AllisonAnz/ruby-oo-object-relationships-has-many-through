b4d59d87942998eabaf2b74380cbd9d0a2169c5d
AllisonAnz <AllisonAnzalone@gmail.com>
2020-12-04 22:59:24 -0600

    commit tree e8dbd7e13a2edeb77d0a14a955c51ec8ac79beb7

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
+    @@all = []
+    #initializes with waiter, a customer, a total and a tip
+    #is a class method that returns the contents of @@all
+    def initialize(waiter, customer, total, tip)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
